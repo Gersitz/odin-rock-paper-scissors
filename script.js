1078fcd852cf464b5295a8861bb793707410738a
Kevin Gersitz <125976146+Gersitz@users.noreply.github.com>
2024-07-12 18:37:16 +0200

    Create pseudocode and implementation for playable game

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
-
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-console.log(playRound(humanChoice, computerChoice));
+playGame();
 
 function getComputerChoice() {
     let choice = Math.random() * 100;
@@ -14,7 +8,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     let validInput = false;
     let choice = prompt("Pick your weapon: Rock, Paper or Scissors!");
-
+    
     while (!validInput) {
         if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") {
             validInput = true;
@@ -26,14 +20,38 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice.toLowerCase() == computerChoice.toLowerCase()) {
-        return "It's a tie!";
-    } else if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" || humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        return "You win!"
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    let round = (humanChoice, computerChoice) => {
+        if (humanChoice.toLowerCase() == computerChoice.toLowerCase()) {
+            return "It's a tie!";
+        } else if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" || humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            return "You win!"
+        } else {
+            computerScore++;
+            return "You lose!";
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        
+        
+        console.log(round(humanChoice, computerChoice));
+    }
+    
+    if (humanScore == computerScore) {
+        console.log(`You got ${humanScore} points. The computer got ${computerScore} points. It's a tie!`)
+
+    }
+    else if (humanScore > computerScore) {
+        console.log(`You got ${humanScore} points. The computer got ${computerScore} points. You win!`)
     } else {
-        computerScore++;
-        return "You lose!";
+        console.log(`You got ${humanScore} points. The computer got ${computerScore} points. You lose.`)
+        
     }
 }
